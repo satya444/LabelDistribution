2f2ab0c7df803d0af3841c4e7d57da0844c0245a
dilip <dilipbsr90@gmail.com>
2014-09-20 16:23:37 -0500

    forgot to push config.txt pushing it now

---
+++ src/LDMain.java
@@ -0,0 +1,7 @@
+import com.utd.ld.main.AosMain;
+public class LDMain {
+
+	public static void main(String args[]){
+	AosMain.main(args);
+	}
+}
---
+++ src/com/utd/ld/constants/AllConstants.java
@@ -0,0 +1,7 @@
+package com.utd.ld.constants;
+
+public class AllConstants {
+
+	public static final String basicConfigFile="/home/dilip/LabelDistribution/LabelDistribution/basicConfig.txt";
+	public static final String configFile= "/home/dilip/LabelDistribution/LabelDistribution/config.txt";
+}
